0f57e8e92bd34a0dd0090617973792f74911ef22
Amy-Kate Andrews <amy.kate.andrews@gmail.com>
2019-01-06 12:00:44 -0700

    adding new mutation methods

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -10,6 +10,31 @@ const Mutations = {
             info
         );
         return item;
+    },
+    async updateItem(parent, args, context, info) {
+        // first take a copy of the updates
+        const updates = { ...args };
+        // remove the ID from the updates
+        delete updates.id;
+        // run the update method
+        return await context.db.mutation.updateItem(
+            {
+                data: updates,
+                where: {
+                    id: args.id
+                }
+            },
+            info
+        );
+    },
+
+    async deleteItem(parent, args, context, info) {
+        const where = { id: args.id };
+        // 1. find item
+        const item = await context.db.query.item({ where }, `{ id title}`);
+        // 2. TODO: check if they own that item, or have the permissions
+        // 3. delete it
+        return await context.db.mutation.deleteItem({ where }, info);
     }
 };
 
